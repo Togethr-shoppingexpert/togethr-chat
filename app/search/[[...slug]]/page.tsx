560c6d1dffc9cb23cef4d635d2c684e808aee0e5
R3GT-hub <saransh2002sharma@gmail.com>
2024-05-27 15:54:55 +0530

    autoscroll

--- app/search/[[...slug]]/page.tsx
+++ app/search/[[...slug]]/page.tsx
@@ -533,7 +533,7 @@ if (isPageRefreshed) {
   }, []);
   
   return (
-    <main className={`${isDarkMode ? "bg-[#202222]" : "bg-[#dde7eb]"} z-[100] mt-[40px] min-h-[100vh]`}>
+    <main className={`${isDarkMode ? "bg-[#202222]" : "bg-[#dde7eb]"} z-[100] min-h-[100vh]`}>
       
       <Navbar mode={isDarkMode?"dark":"light"} />
       <div className=" fixed top-[90px] right-4">
