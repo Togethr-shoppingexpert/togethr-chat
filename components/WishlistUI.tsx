20b2ff0bc9c39dd494d12c914ec1c654f384fe1d
Mehakpreet417 <chopramehakpreet@gmail.com>
2024-08-22 21:17:10 +0530

    style

--- components/WishlistUI.tsx
+++ components/WishlistUI.tsx
@@ -60,7 +60,7 @@ export default function WishlistUI({
     <>
       {isContentAvailable && (
         <div className="flex flex-col gap-y-6 pt-8 mt-0 items-center px-4 pb-10 w-[100%] justify-center">
-          <div className="text-2xl w-full pl-10 font-bold text-white">
+          <div className="text-2xl w-full pl-20 font-bold text-white">
             <h4>Your Wishlist</h4>
           </div>
           {productReviews.map((item, index) => {
@@ -89,7 +89,7 @@ export default function WishlistUI({
                       <div className="text-[17px] text-white">
                         {item.review.split("\n")[0]} {/* Display product name */}
                       </div>
-                      <div className="flex h-max items-center gap-x-2 p-1.5 px-3 rounded-xl bg-[#E8DEF8]">
+                      <div className="flex h-max items-center gap-x-2 p-1.5 px-3 mt-2 rounded-xl bg-[#E8DEF8]">
                         <div className="w-3">
                           <Image src={BlackTick} alt="tick" />
                         </div>
@@ -106,9 +106,9 @@ export default function WishlistUI({
                   </div>
                 </a>
               </Link>
-              <div  className=" absolute right-5 top-2 z-10">
-                 <button className="text-[#a7a7a7] absolute right-2 top-2 z-10" onClick={() => onDelete(item.productId)}>
-                    <span>Remove</span>
+              <div  className=" absolute right-1 top-1 z-10">
+                 <button className="hover:text-black rounded-lg shadow-lg border-2 border-[#f5f5f58a]  hover:bg-[#f5f5f58a] p-1 text-white absolute right-2 top-2 z-10" onClick={() => onDelete(item.productId)}>
+                    <span className="font-semibold text-sm">Remove</span>
                   </button>
               </div>
               </div>
