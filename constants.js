9c4909227d4f3950676fe88bfaf1cbd65ac4b1a3
Ankit Maurya <77996722+ankitmr27@users.noreply.github.com>
2024-09-14 15:33:19 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -6,9 +6,9 @@ const development = {
 };
 
 //export const config = process.env.NODE_ENV === 'development' ? production : development;
-// export const config = process.env.NODE_ENV === 'development' ? development : production;
+export const config = process.env.NODE_ENV === 'development' ? development : production;
 
-export const config = development;
+// export const config = development;
 //export const config =process.env.NODE_ENV === "development" ? production : development;
  // export const config = process.env.NODE_ENV === 'development' ? development : production;
 console.log('config console', config.url);
